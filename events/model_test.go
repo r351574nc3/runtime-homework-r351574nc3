34329d76f27ec6aa26b8179067b60f3780f4d66c
Leo Przybylski <r351574nc3@gmail.com>
2018-06-14 19:30:06 -0700

    Testing loading events

---
+++ events/model_test.go
@@ -0,0 +1,16 @@
+package events
+
+import (
+	"os"
+	"testing"
+)
+
+func TestLoadEvents(t *testing.T) {
+	os.Setenv("EVENT_FILE", "test_data/events.json")
+	events := LoadEvents()
+
+	expected := 24
+	if len(events) != expected {
+		t.Errorf("Expected %d events but got %d", expected, len(events))
+	}
+}
